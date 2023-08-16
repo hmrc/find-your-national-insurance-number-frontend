3afc05ce1b5426b9d737dc59b91409829f41d360
Katherine Boyfield <44240290+Kitkatann@users.noreply.github.com>
2023-08-16 11:29:18 +0100

    ITS-2837 Provided details from PDV in CheckDetailsController (#27)
    * ITS-2837 Provided details from PDV in CheckDetailsController
    
    * ITS-2837 Fixing unit tests.
    
    * ITS-2837 Added timeout for PDV repository.
    
    * ITS-2837 Fixing build error.
    
    ---------
    
    Co-authored-by: rambabu-posa <rambabu.posa@digital.hmrc.gov.uk>
    Co-authored-by: Rishi Sachania <rishisachania@gmail.com>

--- app/controllers/CheckDetailsController.scala
+++ app/controllers/CheckDetailsController.scala
@@ -17,6 +17,10 @@
 package controllers
 
 import config.FrontendAppConfig
+import play.api.Logging
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.PersonalDetailsValidationService
 import connectors.IndividualDetailsConnector
 
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
@@ -26,41 +30,56 @@ import models.{IndividualDetailsResponseEnvelope, CorrelationId, IndividualDetai
 import models.individualdetails.{IndividualDetails, ResolveMerge}
 import models.Mode
 
-import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-
 import uk.gov.hmrc.crypto.SymmetricCryptoFactory
 import uk.gov.hmrc.http.HeaderCarrier
+
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 
 import java.util.UUID
 import javax.inject.Inject
 
 import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
 class CheckDetailsController @Inject()(
-                                        override val messagesApi: MessagesApi,
-                                        identify: IdentifierAction,
-                                        getData: DataRetrievalAction,
-                                        requireData: DataRequiredAction,
-                                        individualDetailsConnector:         IndividualDetailsConnector,
-                                        val controllerComponents: MessagesControllerComponents
-                                      )(implicit ec: ExecutionContext, appConfig: FrontendAppConfig) extends FrontendBaseController with I18nSupport {
+    override val messagesApi: MessagesApi,
+    identify: IdentifierAction,
+    getData: DataRetrievalAction,
+    requireData: DataRequiredAction,
+    personalDetailsValidationService: PersonalDetailsValidationService,
+    individualDetailsConnector: IndividualDetailsConnector,
+    val controllerComponents: MessagesControllerComponents
+  )(implicit ec: ExecutionContext, appConfig: FrontendAppConfig) extends FrontendBaseController with I18nSupport with Logging {
+
 
   def onPageLoad(mode: Mode, validationId: String): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
       // TODO Step 1:- PDV Validation logic
       // TODO Step 2:- API 1694 integration
 
-
-      val t: IndividualDetailsResponseEnvelope[IndividualDetails] = getIndividualDetails(IndividualDetailsNino("AB123456C"))
+      personalDetailsValidationService.createPDVFromValidationId(validationId).onComplete {
+        case Success(value) => {
+          logger.info(value)
+          personalDetailsValidationService.getPersonalDetailsValidationByValidationId(validationId).onComplete {
+            case Success(pdv) => {
+              logger.info("pdv result: " + pdv)
+              val ninoFromPDV = pdv.map(_.personalDetails.map(_.nino).getOrElse(""))
+              val postCodeFromPDV = pdv.map(_.personalDetails.map(_.postCode).getOrElse(""))
+            }
+            case Failure(ex) => logger.warn(ex.getMessage)
+          }
+        }
+        case Failure(ex) => logger.warn(ex.getMessage)
+      }
 
 
       val postCodeMatched = true // TODO expecting flag value after performing these two steps
-      if(postCodeMatched)
+      if(postCodeMatched) {
         Redirect(routes.ValidDataNINOHelpController.onPageLoad())
-      else
+      } else {
         Redirect(routes.InvalidDataNINOHelpController.onPageLoad(mode))
+      }
+
   }
 
 
