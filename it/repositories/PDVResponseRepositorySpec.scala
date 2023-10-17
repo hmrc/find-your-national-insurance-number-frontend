5078d5f124c7bbdb42c0733f90afde0ecec6f288
Shihabur-Atos <98172596+Shihabur-Atos@users.noreply.github.com>
2023-10-17 13:45:39 +0100

    ITS-3014 | PDVResponseRepository IT tests have been fixed and all are passing

--- it/repositories/PDVResponseRepositorySpec.scala
+++ it/repositories/PDVResponseRepositorySpec.scala
@@ -11,6 +11,7 @@ import org.scalatestplus.mockito.MockitoSugar
 import uk.gov.hmrc.domain.{Generator, Nino}
 import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
 
+import java.time.temporal.ChronoUnit
 import java.time.{Instant, LocalDate}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
@@ -24,6 +25,7 @@ class PDVResponseRepositorySpec
     with OptionValues
     with MockitoSugar {
 
+  private val instant = Instant.now.truncatedTo(ChronoUnit.MILLIS)
   private val fakeNino = Nino(new Generator(new Random()).nextNino.nino)
 
   val personalDetails: PersonalDetails =
@@ -35,7 +37,14 @@ class PDVResponseRepositorySpec
       LocalDate.parse("1945-03-18")
     )
 
-  private val pdvResponseData = PDVResponseData("id", "valid", Some(personalDetails), Instant.ofEpochSecond(1))
+  private val pdvResponseData = PDVResponseData(
+    "id", "valid", Some(personalDetails), Instant.ofEpochSecond(1), None, Some("false"), Some("false"))
+
+  private val validCustomerPDVResponseData = PDVResponseData(
+    "id", "valid", Some(personalDetails), Instant.ofEpochSecond(1), Some("Valid Reason"), Some("true"), Some("false"))
+
+  private val invalidCustomerPDVResponseData = PDVResponseData(
+    "id", "valid", Some(personalDetails), Instant.ofEpochSecond(1), Some("Invalid Reason"), Some("false"), Some("false"))
 
   private val mockAppConfig = mock[FrontendAppConfig]
   when(mockAppConfig.cacheTtl) thenReturn 1
@@ -45,17 +54,57 @@ class PDVResponseRepositorySpec
     appConfig = mockAppConfig,
   )
 
+
+  ".updateCustomerValidityWithReason" - {
+
+    "when there is a record for this id" - {
+
+      "must update record to be a valid customer" in {
+
+        val expectedResult = validCustomerPDVResponseData copy (lastUpdated = instant)
+
+        insert(pdvResponseData).futureValue
+
+        repository.updateCustomerValidityWithReason(pdvResponseData.id, validCustomer = true, "Valid Reason").futureValue
+
+        val result = repository.findByValidationId(pdvResponseData.id).futureValue
+
+        result.value.validationStatus mustEqual expectedResult.validationStatus
+        result.value.personalDetails  mustEqual expectedResult.personalDetails
+        result.value.reason           mustEqual expectedResult.reason
+      }
+
+      "must update record to be an invalid customer" in {
+
+        val expectedResult = invalidCustomerPDVResponseData copy (lastUpdated = instant)
+
+        insert(pdvResponseData).futureValue
+
+        repository.updateCustomerValidityWithReason(pdvResponseData.id, validCustomer = false, "Invalid Reason").futureValue
+
+        val result = repository.findByValidationId(pdvResponseData.id).futureValue
+
+        result.value.validationStatus mustEqual expectedResult.validationStatus
+        result.value.personalDetails  mustEqual expectedResult.personalDetails
+        result.value.reason           mustEqual expectedResult.reason
+      }
+    }
+  }
+
   ".findByValidationId" - {
 
     "when there is a record for this id" - {
 
       "must get the record" in {
+
         insert(pdvResponseData).futureValue
 
         val result = repository.findByValidationId(pdvResponseData.id).futureValue
         val expectedResult = pdvResponseData
 
-        result.value mustEqual expectedResult
+        result.value.id               mustEqual expectedResult.id
+        result.value.personalDetails  mustEqual expectedResult.personalDetails
+        result.value.validationStatus mustEqual expectedResult.validationStatus
       }
     }
 
@@ -72,12 +121,15 @@ class PDVResponseRepositorySpec
     "when there is a record for this id" - {
 
       "must get the record" in {
+
         insert(pdvResponseData).futureValue
 
         val result = repository.findByNino(pdvResponseData.personalDetails.get.nino.value).futureValue
         val expectedResult = pdvResponseData
 
-        result.value mustEqual expectedResult
+        result.value.id mustEqual expectedResult.id
+        result.value.personalDetails mustEqual expectedResult.personalDetails
+        result.value.validationStatus mustEqual expectedResult.validationStatus
       }
     }
 
