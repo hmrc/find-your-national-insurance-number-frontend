449b4d29b30ee76abe8b0409c6634020526babdb
rambabu-posa <rambabu.posa@digital.hmrc.gov.uk>
2024-02-20 12:25:50 +0000

    ITS-3295 Added IT tests.

--- it/repositories/EncryptedIndividualDetailsRepositoryISpec.scala
+++ it/repositories/EncryptedIndividualDetailsRepositoryISpec.scala
@@ -1,16 +1,25 @@
-
-
-package repositories
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package repositories
 
 import config.FrontendAppConfig
 import models.encryption.id.EncryptedIndividualDetailsDataCache
 import models.individualdetails.{IndividualDetailsData, IndividualDetailsDataCache}
-import org.mockito.IdiomaticMockito.StubbingOps
 import org.mockito.Mockito.when
-import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
-import org.mongodb.scala.model.Filters
 import org.scalatest.OptionValues
 import org.scalatest.RecoverMethods.recoverToExceptionIf
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
@@ -18,16 +27,11 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.mockito.MockitoSugar
 import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
-import org.mockito.Mockito.verify
-import org.mockito.MockitoSugar.doReturn
-import org.scalacheck.Prop.exception
 
 import java.time.Instant
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
 import play.api.Logging
-import uk.gov.hmrc.mongo.MongoComponent
+
 class EncryptedIndividualDetailsRepositoryISpec
   extends AnyFreeSpec
     with Matchers
--- it/repositories/IndividualDetailsRepositoryISpec.scala
+++ it/repositories/IndividualDetailsRepositoryISpec.scala
@@ -1,9 +1,22 @@
-
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package repositories
 
 import config.FrontendAppConfig
-import models.encryption.id.EncryptedIndividualDetailsDataCache
 import models.individualdetails.{IndividualDetailsData, IndividualDetailsDataCache}
 import org.mockito.Mockito.when
 import org.scalatest.OptionValues
@@ -17,6 +30,7 @@ import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
 
 import java.time.Instant
 import scala.concurrent.ExecutionContext.Implicits.global
+
 class IndividualDetailsRepositoryISpec
   extends AnyFreeSpec
     with Matchers
@@ -26,8 +40,7 @@ class IndividualDetailsRepositoryISpec
     with OptionValues
     with MockitoSugar with Logging {
 
-
-  val mockAppConfig = mock[FrontendAppConfig]
+  val mockAppConfig: FrontendAppConfig = mock[FrontendAppConfig]
   when(mockAppConfig.cacheTtl) thenReturn 1
 
   protected override val repository = new IndividualDetailsRepository(
@@ -68,7 +81,6 @@ class IndividualDetailsRepositoryISpec
       }
     }
 
-
     "when an exception is thrown" - {
 
       "must log an error and return a failed future" in {
@@ -83,9 +95,6 @@ class IndividualDetailsRepositoryISpec
       }
     }
 
-
   }
-
-
 
 }
