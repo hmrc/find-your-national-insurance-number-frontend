9af89419d2d276e529d640530ddb6ac17d069298
rishisachania <rishisachania@gmail.com>
2024-03-04 10:42:19 +0000

    work in progress

---
+++ it/connectors/DefaultIndividualDetailsConnectorSpec.scala
@@ -0,0 +1,105 @@
+
+
+package connectors
+
+import connectors._
+import models._
+import models.individualdetails._
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.test.{DefaultAwaitTimeout, Injecting}
+import uk.gov.hmrc.http.client.HttpClientV2
+import base.WireMockHelper
+import config.{DesApiServiceConfig, FrontendAppConfig}
+import models.IndividualDetailsResponseEnvelope.IndividualDetailsResponseEnvelope
+import org.mockito.MockitoSugar.{mock, when}
+import org.mockito.MockitoSugar.when
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.domain.{Generator, Nino}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import uk.gov.hmrc.play.bootstrap.metrics.Metrics
+
+import java.util.UUID
+import scala.util.Random
+
+
+
+
+class DefaultIndividualDetailsConnectorSpec
+  extends ConnectorSpec
+    with WireMockHelper
+    with MockitoSugar
+    with DefaultAwaitTimeout
+    with Injecting {
+
+
+  val mockHttpClient: HttpClient = mock[HttpClient]
+  val config: FrontendAppConfig = mock[FrontendAppConfig]
+  val metrics: Metrics = mock[Metrics]
+
+
+  // Create a mock instance of DesApiServiceConfig
+  val mockDesApiServiceConfig = MockitoSugar.mock[DesApiServiceConfig]
+
+
+
+  override implicit lazy val app: play.api.Application = new GuiceApplicationBuilder()
+    .overrides(
+      bind[HttpClient].toInstance(mockHttpClient),
+      bind[FrontendAppConfig].toInstance(config),
+      bind[Metrics].toInstance(metrics)
+    )
+    // Bind the mock HttpClient
+    .configure("external-url.individual-details-service.port" -> server.port())
+    .build()
+
+
+  val identifier:IndividualDetailsIdentifier = IndividualDetailsNino(new Generator(new Random()).nextNino.nino)
+  val resolveMerge = ResolveMerge('Y')
+
+  trait SpecSetup {
+    def url(identifier: IndividualDetailsIdentifier, resolveMerge: ResolveMerge): String
+
+    lazy val connector = {
+      val httpClient = app.injector.instanceOf[HttpClient]
+      val config = app.injector.instanceOf[FrontendAppConfig]
+      val metrics = app.injector.instanceOf[Metrics]
+      new DefaultIndividualDetailsConnector(httpClient, config, metrics)
+    }
+  }
+
+  "DefaultIndividualDetailsConnector" must {
+
+    trait LocalSetup extends SpecSetup {
+      def url(identifier: IndividualDetailsIdentifier, resolveMerge: ResolveMerge) = s"/individuals/details/NINO/${identifier.value}/${resolveMerge.value}"
+    }
+
+    "return IndividualDetails when called with a valid identifier and resolveMerge" in new LocalSetup {
+
+      when(mockDesApiServiceConfig.token).thenReturn("yourToken")
+      when(mockDesApiServiceConfig.environment).thenReturn("yourEnvironment")
+      when(mockDesApiServiceConfig.originatorId).thenReturn("yourOriginatorId")
+
+      // Create an instance of HeaderCarrier
+      implicit val hc: HeaderCarrier = HeaderCarrier()
+
+
+
+      // Call the desApiHeaders function
+      val headers: HeaderCarrier = connectors.desApiHeaders(mockDesApiServiceConfig)(hc, correlationId)
+
+
+      when(config.individualDetailsServiceUrl)
+        .thenReturn(s"http://localhost:${server.port()}")
+
+
+
+      implicit val correlationId = CorrelationId(UUID.randomUUID())
+      stubGet(url(identifier, resolveMerge), OK, Some(""))
+      val result = connector.getIndividualDetails(identifier, resolveMerge)
+      result mustBe a[IndividualDetailsResponseEnvelope[IndividualDetails]]
+    }
+
+    // Add more test cases as needed
+  }
+}
