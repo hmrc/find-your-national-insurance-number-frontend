e6a70b47597d7e928b0679a03561cad028b91198
rishisachania <rishisachania@gmail.com>
2024-03-07 11:27:27 +0000

    work in progress

--- test/connectors/DefaultIndividualDetailsConnectorSpec.scala
+++ test/connectors/DefaultIndividualDetailsConnectorSpec.scala
@@ -20,68 +20,81 @@ import com.codahale.metrics.MetricRegistry
 import config.{DesApiServiceConfig, FrontendAppConfig}
 import connectors.HttpReadsWrapper.Recovered
 import models.errors.{ConnectorError, IndividualDetailsError}
-import models.individualdetails.ResolveMerge
-import models.upstreamfailure.UpstreamFailures
-import org.mockito.Mockito.{mock, _}
-import org.scalatestplus.mockito.MockitoSugar
-import org.scalatestplus.play.PlaySpec
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
-import models.{CorrelationId, IndividualDetailsNino}
+import models.individualdetails.{IndividualDetails, ResolveMerge}
+import models.{CorrelationId, IndividualDetailsNino, IndividualDetailsResponseEnvelope}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchersSugar.eqTo
+import org.mockito.Mockito._
 import org.mockito.MockitoSugar.when
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
 import play.api.Logger
-import uk.gov.hmrc.play.bootstrap.metrics.Metrics
+import play.api.http.Status.INTERNAL_SERVER_ERROR
+import services.CheckDetailsServiceSpec.fakeIndividualDetails
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpReads}
+import uk.gov.hmrc.play.bootstrap.metrics._
 
+import java.net.URL
 import java.util.UUID
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 class DefaultIndividualDetailsConnectorSpec extends PlaySpec with MockitoSugar {
 
   "getIndividualDetails" should {
-    "make a GET request to the correct URL" in {
-      val mockHttpClient = mock[HttpClient]
-      val mockAppConfig = mock[FrontendAppConfig]
-      val metrics = mock[Metrics]
+    "make a GET request and return correct results" in {
+
+      implicit val hc: HeaderCarrier = HeaderCarrier()
+      implicit val correlationId: CorrelationId = CorrelationId(UUID.randomUUID())
 
       val testIdentifier = IndividualDetailsNino("AB123456C")
       val testResolveMerge = ResolveMerge('Y')
-      val testUrl = s"/individuals/details/NINO/${testIdentifier.value}/${testResolveMerge.value}"
+      val testUrl = s"http://localhost:9000/individuals/details/NINO/${testIdentifier.value}/${testResolveMerge.value}"
+
+      val mockHttpClient = mock[HttpClient]
+      val mockAppConfig = mock[FrontendAppConfig]
+      val metrics = mock[Metrics]
+      val mockAdditionalLogInfo = AdditionalLogInfo(Map("correlation-id" -> "1234"))
+      val mockLogger = mock[Logger]
+      val mockConnectorName = "individual-details-connector"
+      val mockMetrics = mock[uk.gov.hmrc.play.bootstrap.metrics.Metrics]
       val mockDesApiServiceConfig = mock[DesApiServiceConfig]
+      val mockHttpReads = mock[HttpReads[Either[IndividualDetailsError, IndividualDetails]]]
+
       when(mockDesApiServiceConfig.token).thenReturn("yourToken")
       when(mockDesApiServiceConfig.environment).thenReturn("yourEnvironment")
       when(mockDesApiServiceConfig.originatorId).thenReturn("yourOriginatorId")
 
       when(mockAppConfig.individualDetailsServiceUrl).thenReturn("http://localhost:9000")
       when(mockAppConfig.individualDetails).thenReturn(mockDesApiServiceConfig)
-      // Create a mock instance of Future[Either[IndividualDetailsError, T]]
-      //val mockHttpResult = mock[Future[Either[IndividualDetailsError, String]]]
-
-      // Define the behavior of the `recover` method
-      //when(mockHttpResult.recover(any())(any())).thenReturn(Future.successful(Right("Success")))
-
-      when(mockHttpClient.GET[HttpResponse](any())(any(), any(), any()))
-        .thenReturn(Future.successful(HttpResponse(200, "Success")))
-
-      // Create a mock instance of Recovered
-      implicit val mockRecovered = mock[Recovered[String]]
-
-      // Define the behavior of the `recovered` method
-      when(mockRecovered.recovered(any[Logger], any[String], any[MetricRegistry], any[Option[AdditionalLogInfo]])(any[ExecutionContext]))
-        .thenReturn(Future.successful(Right("Success")))
-
-      implicit val hc: HeaderCarrier = HeaderCarrier()
-      implicit val correlationId: CorrelationId = CorrelationId(UUID.randomUUID())
-
-      val connector = new DefaultIndividualDetailsConnector(mockHttpClient, mockAppConfig, metrics) with HttpReadsWrapper[UpstreamFailures, IndividualDetailsError] {
-        override def fromUpstreamErrorToIndividualDetailsError(connectorName: String, status: Int, upstreamError: UpstreamFailures, additionalLogInfo: Option[AdditionalLogInfo]): ConnectorError =
-          ???
-      }
+      when(mockMetrics.defaultRegistry).thenReturn(new MetricRegistry())
 
 
-      connector.getIndividualDetails(testIdentifier, testResolveMerge)(global, hc, correlationId)
-      verify(mockHttpClient).GET(eqTo(testUrl))(any(), any(), any())
+      //      when(mockHttpReads.read(any(), any(), any())).thenReturn(
+      //        (Left(ConnectorError(INTERNAL_SERVER_ERROR, s"downstream error with status code"))))
+
+      //            when(mockHttpReads.read(any(), any(), any())).thenReturn(
+      //              (Right(fakeIndividualDetails)))
+
+      when(mockHttpClient.GET[Either[IndividualDetailsError, IndividualDetails]](new URL(testUrl))(mockHttpReads, hc, global))
+        .thenReturn(
+          Future.successful(Right(fakeIndividualDetails))
+        )
+
+      when(mockHttpClient.GET[Either[IndividualDetailsError, IndividualDetails]](new URL(testUrl))(mockHttpReads, hc, global)
+        .recovered(mockLogger, mockConnectorName, mockMetrics.defaultRegistry, Some(mockAdditionalLogInfo))(global))
+          .thenReturn(
+            Future.successful(Left(ConnectorError(INTERNAL_SERVER_ERROR, s"downstream error with status code")))
+          )
+
+
+      val connector = new DefaultIndividualDetailsConnector(mockHttpClient, mockAppConfig, metrics)
+
+      val res = connector.getIndividualDetails(testIdentifier, testResolveMerge)
+      res mustBe IndividualDetailsResponseEnvelope(Right(fakeIndividualDetails))
+
+      //verify(mockHttpClient).GET[Either[IndividualDetailsError, IndividualDetails]](new URL(testUrl))(mockHttpReads, hc, global)
+      //
     }
   }
 }
