ccdd0b2826048d3f5e1c29be75f589afba1e5785
rambabu-posa <rambabu.posa@digital.hmrc.gov.uk>
2024-03-08 11:45:11 +0000

    PlatBotSuggestions: Removed some configs from application.conf. (#166)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,16 +5,16 @@ object AppDependencies {
   private val playVersion = "play-28"
   private val bootstrapVersion = "8.1.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"      % s"1.12.0-$playVersion",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"           % "0.71.0",
     "uk.gov.hmrc"       %% s"sca-wrapper-$playVersion"          % "1.3.0",
     "org.typelevel"     %% "cats-core"                          % "2.7.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-28"                % "7.6.0"
+    "uk.gov.hmrc"       %% s"crypto-json-$playVersion"          % "7.6.0"
 )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "org.scalatestplus"    %% "mockito-4-6"                    % "3.2.15.0",
     "org.mockito"          %% "mockito-scala"                  % "1.16.42",
     "org.scalatestplus"    %% "scalacheck-1-17"                % "3.2.15.0",
