c4fd9ba32956697c3b5b2ea4eb49f771d2991442
grantrobson <grant.robson@digital.hmrc.gov.uk>
2025-02-03 14:33:29 +0000

    9796 Fix unit tests

--- app/models/OriginType.scala
+++ app/models/OriginType.scala
@@ -29,14 +29,13 @@ object OriginType extends Enumerable.Implicits {
   case object FMN extends WithName("FMN") with OriginType
 
   val values: Seq[OriginType] = Seq(PDV, IV, FMN)
-  
-  def toFeedbackSource(originType: OriginType): String = {
-  originType match {
-    case PDV => ""
-    case IV => "IDENTITY_VERIFICATION"
-    case _ => "FIND_MY_NINO"
-  }
-  }
+
+  def toFeedbackSource(originType: OriginType): String =
+    originType match {
+      case PDV => "PERSONAL_DETAILS_VALIDATION"
+      case IV  => "IDENTITY_VERIFICATION"
+      case _   => "FIND_MY_NINO"
+    }
 
   private val mappings: Map[String, OriginType] = values.map(v => (v.toString, v)).toMap
 
